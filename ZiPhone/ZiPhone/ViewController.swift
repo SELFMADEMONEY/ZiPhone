64b2fffa33748079871483a863de49d2a381dd42
M <98020680+SELFMADEMONEY@users.noreply.github.com>
2022-03-19 09:38:09 -0700

    open all
    opem app

---
+++ ZiPhone/ZiPhone/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZiPhone
+//
+//  Created by SELFMADE on 3/16/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
